25b0d769bdec17eb8f0806ec62d36f3d44c9ec21
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-09-28 14:24:29 +0300

    Set dynamic application port using ConfigService
    Previously, the application port was hardcoded to 3000. Now, the port is dynamically set using the ConfigService, allowing for flexible configuration based on different environments.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const appPort = configService.get("PORT")
+  console.log('App is running on port:', appPort);
+  await app.listen(appPort);
 }
 bootstrap();

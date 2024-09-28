a02947cf613a0d62f42eff515a6d83d068332892
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-09-28 14:40:02 +0300

    Enable CORS with dynamic origin for development
    Added a conditional CORS configuration to allow requests from the frontend domain when in development mode. This ensures that only the specified frontend domain can interact with the backend during development while keeping CORS open-ended in production. This setup enhances security and debugging efficiency.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const appPort = configService.get("PORT")
+  const isDev = configService.get<string>('NODE_ENV') !== 'production';
+  const frontendDomain = configService.get<string>('FRONTEND_DOMAIN');
+
+  app.enableCors({
+    origin: isDev ? [frontendDomain] : undefined,
+    methods: ["GET","HEAD", "PUT", "POST", "DELETE"],
+    allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin', 'Authorization'],
+    credentials: true,
+  });
+
   console.log('App is running on port:', appPort);
   await app.listen(appPort);
 }

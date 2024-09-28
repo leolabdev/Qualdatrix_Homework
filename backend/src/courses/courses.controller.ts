e654d5e313afc9efc70e4c6083b3b03d987f06e5
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-09-28 18:21:52 +0300

    Add API documentation to CoursesController methods
    Included detailed API operation summaries and response descriptions for all methods in CoursesController. This enhances clarity and consistency in the API documentation, aiding both development and user experience.

--- backend/src/courses/courses.controller.ts
+++ backend/src/courses/courses.controller.ts
@@ -3,27 +3,41 @@ import { CoursesService } from './courses.service';
 import { Course } from './course.entity';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CourseResponseBasicDto } from './dto/course-response-basic.dto';
 
+@ApiTags('courses')
 @Controller('courses')
 export class CoursesController {
   constructor(private readonly coursesService: CoursesService) {}
 
   @Get()
-  async getAllCourses(): Promise<Course[]> {
+  @ApiOperation({ summary: 'Get all courses' })
+  @ApiResponse({ status: 200, description: 'List of courses', type: [CourseResponseBasicDto] })
+  async getAllCourses(): Promise<CourseResponseBasicDto[]> {
     return this.coursesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a course by ID' })
+  @ApiResponse({ status: 200, description: 'The found course', type: Course })
+  @ApiResponse({ status: 404, description: 'Course not found' })
   async getCourseById(@Param('id', ParseIntPipe) id: number): Promise<Course> {
     return this.coursesService.findOne(id);
   }
 
   @Post()
-  async createCourse(@Body() createCourseDto: CreateCourseDto): Promise<Course> {
+  @ApiOperation({ summary: 'Create a new course' })
+  @ApiResponse({ status: 201, description: 'The course has been created', type: CourseResponseBasicDto })
+  @ApiResponse({ status: 409, description: 'Course title already exists' })
+  async createCourse(@Body() createCourseDto: CreateCourseDto): Promise<CourseResponseBasicDto> {
     return this.coursesService.create(createCourseDto);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a course' })
+  @ApiResponse({ status: 200, description: 'The updated course', type: Course })
+  @ApiResponse({ status: 404, description: 'Course not found' })
   async updateCourse(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateCourseDto: UpdateCourseDto,
@@ -32,6 +46,9 @@ export class CoursesController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a course' })
+  @ApiResponse({ status: 200, description: 'Course has been deleted' })
+  @ApiResponse({ status: 404, description: 'Course not found' })
   async removeCourse(@Param('id', ParseIntPipe) id: number): Promise<void> {
     return this.coursesService.remove(id);
   }

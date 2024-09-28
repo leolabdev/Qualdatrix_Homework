bc54c0dd9df7087182186e583e48656a3b866ae0
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-09-28 14:01:09 +0300

    Integrate TypeORM and ConfigModule
    Add TypeORM configuration using ConfigModule to manage database credentials and environment settings. Removed unnecessary AppController and AppService imports to streamline the AppModule.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const isDevelopment = configService.get<string>('NODE_ENV') !== 'production';
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: isDevelopment, // we should do migration manually in production
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
